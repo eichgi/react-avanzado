f257db9c03ff25e1a1bf62b97e5fbaf0313a11e0
Hiram Guerrero <hiramg90@gmail.com>
2021-01-08 10:47:11 -0600

    adding types & resolver

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 const {ApolloServer, gql} = require('apollo-server');
 
-const server = new ApolloServer({});
+const cursos = [
+  {
+    titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+    tecnologia: 'JavaScript ES6',
+  },
+  {
+    titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+    tecnologia: 'React',
+  },
+  {
+    titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+    tecnologia: 'Node.js'
+  },
+  {
+    titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+    tecnologia: 'React'
+  }
+];
+
+//Schema
+const typeDefs = gql`
+    type Curso {
+        titulo: String
+        tecnologia: String
+    }
+
+    type Query {
+        obtenerCursos: [Curso]
+    }
+`;
+
+//Resolver
+const resolvers = {
+  Query: {
+    obtenerCursos: () => cursos
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 server.listen()
   .then(({url}) => {
