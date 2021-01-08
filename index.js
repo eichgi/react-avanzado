107f48cb77699179867095a6231ac735c7bfc9d5
Hiram Guerrero <hiramg90@gmail.com>
2021-01-08 09:53:45 -0600

    first commit: creating an apollo server

---
+++ index.js
@@ -0,0 +1,8 @@
+const {ApolloServer, gql} = require('apollo-server');
+
+const server = new ApolloServer({});
+
+server.listen()
+  .then(({url}) => {
+    console.log(`Servidor listo en la URL: ${url}`)
+  });
