2035653ee7536f5897b4cf090dcc90f7b9e5598b
Hiram Guerrero <hiramg90@gmail.com>
2021-01-08 11:22:02 -0600

    First steps with Apollo & GraphQL

--- index.js
+++ index.js
@@ -1,46 +1,17 @@
-const {ApolloServer, gql} = require('apollo-server');
-
-const cursos = [
-  {
-    titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
-    tecnologia: 'JavaScript ES6',
-  },
-  {
-    titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
-    tecnologia: 'React',
-  },
-  {
-    titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
-    tecnologia: 'Node.js'
-  },
-  {
-    titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
-    tecnologia: 'React'
-  }
-];
-
-//Schema
-const typeDefs = gql`
-    type Curso {
-        titulo: String
-        tecnologia: String
-    }
-
-    type Query {
-        obtenerCursos: [Curso]
-    }
-`;
-
-//Resolver
-const resolvers = {
-  Query: {
-    obtenerCursos: () => cursos
-  }
-};
+const {ApolloServer} = require('apollo-server');
+const typeDefs = require('./db/schema');
+const resolvers = require('./db/resolvers');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: () => {
+    const myContext = "Hola";
+
+    return {
+      myContext,
+    }
+  }
 });
 
 server.listen()
