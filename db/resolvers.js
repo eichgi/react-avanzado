7bb8f82e3785a7cfe6b8c19c6cc903c327aa449d
Hiram Guerrero <hiramg90@gmail.com>
2021-01-08 17:12:21 -0600

    section 7 & 8 completed: products crud

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,4 +1,5 @@
 const Usuario = require('./../models/Usuario');
+const Producto = require('./../models/Producto');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({path: 'variables.env'});
@@ -14,7 +15,23 @@ const
     Query: {
       obtenerUsuario: async (_, {token}) => {
         return await jwt.verify(token, process.env.JWT_SECRET);
-      }
+      },
+      obtenerProductos: async () => {
+        try {
+          return await Producto.find({});
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      obtenerProducto: async (_, {id}) => {
+        const producto = await Producto.findById(id);
+
+        if (!producto) {
+          throw new Error('El producto no existe.');
+        }
+
+        return producto;
+      },
     },
     Mutation: {
       nuevoUsuario: async (_, {input}) => {
@@ -62,7 +79,40 @@ const
           token: crearToken(usuario, process.env.JWT_SECRET, '24h'),
         }
       },
-    }
+      nuevoProducto: async (_, {input}) => {
+        try {
+          const producto = new Producto(input);
+
+          const resultado = await producto.save();
+
+          return resultado;
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      actualizarProducto: async (_, {id, input}) => {
+        let producto = await Producto.findById(id);
+
+        if (!producto) {
+          throw new Error('El producto no existe.');
+        }
+
+        producto = await Producto.findOneAndUpdate({_id: id}, input, {new: true});
+
+        return producto;
+      },
+      eliminarProducto: async (_, {id}) => {
+        let producto = await Producto.findById(id);
+
+        if (!producto) {
+          throw new Error('El producto no existe.');
+        }
+
+        await producto.delete();
+
+        return "El producto ha sido eliminado";
+      },
+    },
   };
 
 module.exports = resolvers;
--- db/schema.js
+++ db/schema.js
@@ -2,10 +2,6 @@ const {gql} = require('apollo-server');
 
 //Schema
 const typeDefs = gql`
-    type Query {
-        obtenerUsuario(token: String!): Usuario
-    }
-    
     type Usuario {
         id: ID
         nombre: String
@@ -13,26 +9,55 @@ const typeDefs = gql`
         email: String
         creado: String
     }
-    
+
     type Token {
         token: String
     }
-    
+
+    type Producto {
+        id: ID
+        nombre: String
+        existencia: Int
+        precio: Float
+        creado: String
+    }
+
     input UsuarioInput {
         nombre: String!
         apellido: String!
         email: String!
         password: String!
     }
-    
+
+    input ProductoInput {
+        nombre: String!
+        existencia: Int!
+        precio: Float!
+    }
+
     input AutenticarInput {
         email: String!
         password: String!
     }
-    
+
+    type Query {
+        #Usuarios
+        obtenerUsuario(token: String!): Usuario,
+
+        #Productos
+        obtenerProductos: [Producto],
+        obtenerProducto(id: ID!): Producto,
+    }
+
     type Mutation {
+        #Productos
+        nuevoProducto(input: ProductoInput): Producto
+        actualizarProducto(id: ID!, input: ProductoInput): Producto
+        eliminarProducto(id: ID!): String
+
+        #Usuarios
         nuevoUsuario(input: UsuarioInput): Usuario
-        autenticarUsuario(input: AutenticarInput): Token
+        autenticarUsuario(input: AutenticarInput): Token,
     }
 `;
 
